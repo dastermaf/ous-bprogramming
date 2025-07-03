6cc6496af6eda956bc9d85535163e9e22f32990f
dastermaf <kamronboijrov@gmail.com>
2025-07-03 11:25:39 +0500

    Standardized comments for variable declarations, assignments, and output in 教科書/chap01/変数 (1-2)/list0108.c

--- 教科書/chap01/変数 (1-2)/list0108.c
+++ 教科書/chap01/変数 (1-2)/list0108.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int x, y;
+    int x, y; // ｘとyはint型の変数を宣言
 
-    x = 57;
-    y = x + 12;
+    x = 57; // ｘに57を代入
+    y = x + 12; // yにx + 12を代入
 
-    printf("xの値は%dです。\n", x);
-    printf("yの値は%dです。\n", y);
+    printf("xの値は%dです。\n", x); // xの値を表示
+    printf("yの値は%dです。\n", y); // yの値を表示
 
     return 0;
 }
