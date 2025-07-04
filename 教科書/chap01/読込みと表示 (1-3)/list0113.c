e30d6df8ba8d1885b27304aca4690ca5ea7b197e
dastermaf <kamronboijrov@gmail.com>
2025-07-04 09:35:34 +0500

    教科書/chap01/読込みと表示 (1-3): コメントを更新; 出力処理の説明を追加

--- 教科書/chap01/読込みと表示 (1-3)/list0113.c
+++ 教科書/chap01/読込みと表示 (1-3)/list0113.c
@@ -5,11 +5,11 @@
 int main(void) {
     int n1, n2;
 
-    puts("二つの整数を入力してください。");
+    puts("二つの整数を入力してください。"); // printf("ABCDE\n")とほぼ同じ働きします。
     printf("整数１："); scanf("%d", &n1);
     printf("整数２："); scanf("%d", &n2);
 
-    printf("それらの和は%dです。\n", n1 + n2); // 和の表示
+    printf("それらの和は%dです。\n", n1 + n2); // 和を表示
 
     return 0;
 }
