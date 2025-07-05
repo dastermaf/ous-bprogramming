fd10243df094ea349f9bd06ce535d58b43ba4c06
dastermaf <kamronboijrov@gmail.com>
2025-07-05 15:54:44 +0500

    教科書/chap03/if文 (3-1): 入力処理をリファクタリング; 新しい練習プログラムを追加

--- 教科書/chap03/if文 (3-1)/practice/practice0310.c
+++ 教科書/chap03/if文 (3-1)/practice/practice0310.c
@@ -6,12 +6,9 @@ int main() {
     int n1, n2, n3;
 
     puts("二つの整数を入力せよ。");
-    printf("整数A： ");
-    scanf("%d", &n1);
-    printf("整数B： ");
-    scanf("%d", &n2);
-    printf("整数C： ");
-    scanf("%d", &n3);
+    printf("整数A： "); scanf("%d", &n1);
+    printf("整数B： "); scanf("%d", &n2);
+    printf("整数C： "); scanf("%d", &n3);
 
     if (n1 == n2 && n2 == n3) {
         puts("三つの値は等しいです。");
