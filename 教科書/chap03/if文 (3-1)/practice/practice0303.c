c8521dfa583fb08dfdac7f26a920b50e841174e1
dastermaf <kamronboijrov@gmail.com>
2025-07-05 13:43:51 +0500

    教科書/chap03/if文 (3-1): 条件分岐に関する未解決のプログラムを解決; コメントを更新し完成した練習例を追加

--- 教科書/chap03/if文 (3-1)/practice/practice0303.c
+++ 教科書/chap03/if文 (3-1)/practice/practice0303.c
@@ -1,1 +1,18 @@
-// 未解決
+// 注釈が未完成
+
+#include <stdio.h>
+
+int main(void) {
+    int n;
+
+    printf("整数を入力せよ： \n");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        n = -n;
+    }
+
+    printf("絶対値は%dです。\n", n);
+
+    return 0;
+}
