f6af6a48c0cbade0c460bf01b2e1ad958ddee37e
dastermaf <kamronboijrov@gmail.com>
2025-07-05 14:14:47 +0500

    教科書/chap03/if文 (3-1): 条件分岐を実装; コメントと入力処理を修正

--- 教科書/chap03/if文 (3-1)/practice/practice0306.c
+++ 教科書/chap03/if文 (3-1)/practice/practice0306.c
@@ -6,10 +6,12 @@ int main(void) {
     int n1, n2, n3;
 
     puts("二つの整数を入力せよ。");
-
-    printf("整数1： "); scanf("%d", &n1);
-    printf("整数2： "); scanf("%d", &n2);
-    printf("整数3： "); scanf("%d", &n3);
+    printf("整数1： ");
+    scanf("%d", &n1);
+    printf("整数2： ");
+    scanf("%d", &n2);
+    printf("整数3： ");
+    scanf("%d", &n3);
 
     int min = n1;
     min = (n2 < min) ? n2 : min;
