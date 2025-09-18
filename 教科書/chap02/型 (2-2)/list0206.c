fdc7609714cefbb1de90c13d0ce2ee2cd753c56f
dastermaf <116442860+dastermaf@users.noreply.github.com>
2025-09-18 17:23:45 +0500

    ディレクトリ変更、応用プログラミングの科目を追加

--- 教科書/chap02/型 (2-2)/list0206.c
+++ 教科書/chap02/型 (2-2)/list0206.c
@@ -1,6 +1,6 @@
 // 整数と浮動小数点数
 
-#include <stdio.h>
+#include <stdio.h>-
 
 int main(void) {
     int n; // 整数
@@ -9,11 +9,11 @@ int main(void) {
     n = 9.99;
     x = 9.99;
 
-    printf("int 型変数の値:%d\n", n);
-    printf("         n/2: %d\n", n / 2);
+    printf("int   型変数 nの値  ：%d\n", n);
+    printf("            n/2   ：%d\n", n / 2);
 
-    printf("double型変数xの値:%f\n", x);
-    printf("          x/2.0: %f\n", x / 2.0);
+    printf("double型変数 xの値  ：%f\n", x);
+    printf("            x /2.0：%f\n", x / 2.0);
 
     return 0;
 }
